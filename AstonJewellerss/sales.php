80bc38b5f547a069227a5fb844102da5bbe1d45d
noortxlib <116088887+noortxlib@users.noreply.github.com>
2022-11-22 14:46:35 +0000

    updated php files

---
+++ AstonJewellerss/sales.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Aston Jewellers</title>
+  <link rel="icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+  <script src="https://kit.fontawesome.com/c54d8b2ca0.js" crossorigin="anonymous"></script>
+  <script defer src="js/main.js"></script>
+</head>
+
+<body>
+  <header id="main-header">
+    <div class="topnav" id="nav">
+      <div class="container-fluid">
+        <div>
+          <a href="#"><i class="fa fa-search"></i> Search</a>
+        </div>
+        <div>
+          <h1>Aston Jewellers</h1>
+          <p>Selling high-quality affordable jewellery</p>
+        </div>
+        <div class="topnav-right">
+          <a href="login.php"><i class="fas fa-user-circle"></i> Log In</a>
+
+        </div>
+      </div>
+      <nav>
+        <div class="topnav-right">
+          <a href="wishlist.php"><i class="fa fa-heart-o"></i></a>
+          <a href="basket.php"><i class="fa fa-shopping-cart"></i></a>
+        </div>
+        <div id="topnav-centered">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="#">Men's</a></li>
+            <li><a href="sales.php">Sale</a></li>
+            <li><a href="about-us.php">About Us</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <h2>Check out our sale today!</h2>
+
+    <body>
+      <div class="gallery">
+        <div class="content">
+          <div class="zoom">
+            <img src="images/diamondearrings.jpg" alt="round-cut-stud-earrings-silver" />
+          </div>
+          <h3>Earrings</h3>
+          <p>Round Cut Stud Earrings 6mm-Silver</p>
+          <h6>£12.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <div class="zoom">
+            <img src="images/pearlearrings.jpg" alt="gold-huggie-pearl-drop-earrings" />
+          </div>
+          <h3>Earrings</h3>
+          <p> Gold Huggie Pearl Drop Earrings</p>
+          <h6>£18.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <div class="zoom">
+            <img src="images/pearlearrings2.jpg" alt="ines-pearl-and-Cz-drop-earrings-rose-gold">
+          </div>
+          <h3>Earrings</h3>
+          <p> Ines Pearl And Cz Drop Earrings-Rose Gold</p>
+          <h6>£14.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <div class="zoom">
+            <img src="images/starearrings.jpg" alt="starbusrt-small-hoop-earrings" />
+          </div>
+          <h3>Earrings</h3>
+          <p>Starburst Small Hoop Earrings-Rose Gold</p>
+          <h6>£11.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/silverchain.jpg" alt="gold-chunky-pendant-necklace" />
+          <h3>Necklace</h3>
+          <p>Gold Chuncky Pendant Necklace</p>
+          <h6>£11.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/heartchain.jpg" alt="gold-dimante-necklace" />
+          <h3>Necklace</h3>
+          <p>Gold Dimante Necklace</p>
+          <h6>£12.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/pearlchain.jpg" alt="little-sea-shell-necklace" />
+          <h3>Necklace</h3>
+          <p>Little Sea Shell Necklace</p>
+          <h6>£9.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/salechain.jpg" alt="rose-gold-necklace" />
+          <h3>Necklace</h3>
+          <p>Rose Gold Necklace</p>
+          <h6>£10.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/diamondring.jpg" alt="diamond-ring" />
+          <h3>Ring</h3>
+          <p>Single Diamond Ring</p>
+          <h6>£12.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/goldring.jpg" alt="gold-ring" />
+          <h3>Ring</h3>
+          <p>Gold Princess Ring</p>
+          <h6>£15.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/goldring2.jpg" alt="detailed-gold-ring" />
+          <h3>Ring</h3>
+          <p>Detailed Gold Ring</p>
+          <h6>£10.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/silverrings.jpg" alt="silver-ring" />
+          <h3>Ring</h3>
+          <p>Silver Diamond Ring</p>
+          <h6>£11.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/salebraclet2.jpg" alt="worded-bracelet" />
+          <h3>Bracelet</h3>
+          <p>Worded Gold Bracelet</p>
+          <h6>£15.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/salebraclet3.jpg" alt="silver-bracelet" />
+          <h3>Bracelet</h3>
+          <p>Silver Bracelet</p>
+          <h6>£9.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/salebraclet4.jpg" alt="gold-chunky-bracelet" />
+          <h3>Bracelet</h3>
+          <p>Gold Chunky Bracelet</p>
+          <h6>£20.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+        <div class="content">
+          <img src="images/salebraclet5.jpg" alt="patterned-gold-bracelet" />
+          <h3>Bracelet</h3>
+          <p>Patterned Gold Bracelet</p>
+          <h6>£18.00</h6>
+          <button class="buy-1">Add to Basket</button>
+        </div>
+
+  </main>
+</body>
+
+</html>
+
+<?php
+include('footer.php');
+?>
---
+++ AstonJewellerss/shop.php
@@ -0,0 +1,476 @@
+<?php
+$product = "";
+if (isset($_GET['product'])) {
+  $product = $_GET['product'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Aston Jewellers</title>
+  <link rel="icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="css/shop.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+  <script src="https://kit.fontawesome.com/c54d8b2ca0.js" crossorigin="anonymous"></script>
+  <script defer src="js/main.js"></script>
+</head>
+
+<body>
+<header id="main-header">
+      <div class="topnav" id="nav">
+        <div class="container-fluid">
+          <div>
+            <a href="#"><i class="fa fa-search"></i> Search</a>
+          </div>
+          <div>
+            <h1>Aston Jewellers</h1>
+            <p>Selling high-quality affordable jewellery</p>
+          </div>
+          <div class="topnav-right">
+            <a href="login.php"><i class="fas fa-user-circle"></i> Log In</a>
+           
+          </div>
+        </div>
+        <nav>
+          <div class="topnav-right">
+            <a href="wishlist.php"><i class="fa fa-heart-o"></i></a>
+            <a href="basket.php"><i class="fa fa-shopping-cart"></i></a>
+          </div>
+          <div id="topnav-centered">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="shop.php">Shop</a></li>
+              <li><a href="#">Men's</a></li>
+              <li><a href="sales.php">Sale</a></li>
+              <li><a href="about-us.php">About Us</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+  <!-- Show store categories when no product is selected (default) -->
+  <?php if (!isset($_GET['product'])) { ?>
+    <div class="wrapper">
+      <div class="image-panel">
+
+        <div class="bracelets">
+          <a href="shop.php?product=bracelets">
+            <h3>Bracelets</h3>
+          </a>
+        </div>
+
+        <div class="necklaces">
+          <a href="shop.php?product=necklaces">
+            <h3>Necklaces</h3>
+          </a>
+        </div>
+
+        <div class="earrings">
+          <a href="shop.php?product=earrings">
+            <h3>Earrings</h3>
+          </a>
+        </div>
+
+        <div class="rings">
+          <a href="shop.php?product=rings">
+            <h3>Rings</h3>
+          </a>
+        </div>
+
+        <div class="watches">
+          <a href="shop.php?product=watches">
+            <h3>Watches</h3>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  <?php } ?>
+  <!-- End of store categories (default) -->
+
+  <!-- Back to other products button -->
+  <?php if (isset($_GET['product'])) { ?>
+    <div class="back"><a href="shop.php" class="previous">&laquo; Browse other products</a></div>
+  <?php } ?>
+
+  <!-- Display bracelets -->
+  <?php if ($product == "bracelets") { ?>
+    <div class="gallery">
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/guardian-angel-bracelet.jpg" alt="guardian-angel-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Silver Guardian Angel Bracelet</p>
+        <h6>£17.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/plain-silver-bracelet.jpg" alt="plain-silver-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Plain Silver Bracelet</p>
+        <h6>£13.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/teardrop-bracelet.jpg" alt="teardrop-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Teardrop Shaped Gemstone Silver Bracelet</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/plain-silver-bangle-bracelet.jpg" alt="plain-silver-bangle-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Plain Silver Bangle</p>
+        <h6>£15.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/silver-butterfly-bracelet.jpg" alt="silver-butterfly-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Silver Butterfly Bracelet</p>
+        <h6>£18.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/oval-cut-gemstone-bracelet.jpg" alt="oval-cut-gemstone-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Oval Cut Gemstone Bracelet</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/bracelets/green-amber-square-gemstone-bracelet.jpg" alt="green-amber-square-gemstone-bracelet" />
+        </div>
+        <h3>Bracelet</h3>
+        <p>Green Amber Square Gemstone Bracelet</p>
+        <h6>£27.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+    </div>
+  <?php } ?>
+  <!-- End of bracelets -->
+
+  <!-- Display necklaces -->
+  <?php if ($product == "necklaces") { ?>
+    <div class="gallery">
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/gold-heart-necklace.jpg" alt="gold-heart-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Gold Heart Necklace</p>
+        <h6>£18.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/gold-four-leaf-clover-necklace.jpg" alt="gold-four-leaf-clover-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Gold Four Leaf Clover Necklace</p>
+        <h6>£18.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/3-heart-necklace.jpg" alt="3-heart-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>3 Heart Necklace</p>
+        <h6>£22.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/flower-pearl-necklace.jpg" alt="flower-pearl-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Flower with Pearl Necklace</p>
+        <h6>£20.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/mermaid-necklace.jpg" alt="mermaid-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Mermaid Necklace</p>
+        <h6>£20.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/hollow-heart-necklace.jpg" alt="hollow-heart-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Hollow Heart Necklace</p>
+        <h6>£16.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/necklaces/poppies-necklace.jpg" alt="poppies-necklace" />
+        </div>
+        <h3>Necklace</h3>
+        <p>Poppies Necklace</p>
+        <h6>£27.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+    </div>
+  <?php } ?>
+  <!-- End of necklaces -->
+
+  <!-- Display earrings -->
+  <?php if ($product == "earrings") { ?>
+    <div class="gallery">
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/emerald-gold-hoop-earrings.jpg" alt="emerald-gold-hoop-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Emerald Gold Hoop Earrings</p>
+        <h6>£24.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/double-hoop-diamond-earrings.jpg" alt="double-hoop-diamond-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Double Hoop Diamond Earrings</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/pearl-earrings.jpg" alt="pearl-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Pearl Earrings</p>
+        <h6>£20.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/diamond-star-earrings.jpg" alt="diamond-star-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Diamond Star Earrings</p>
+        <h6>£27.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/pastel-heart-earrings.jpg" alt="pastel-heart-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Pastel Heart Earrings</p>
+        <h6>£20.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/diamond-gold-round-earrings.jpg" alt="diamond-gold-round-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Diamond & Gold Round Earrings</p>
+        <h6>£30.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/earrings/flower-earrings.jpg" alt="flower-earrings" />
+        </div>
+        <h3>Earrings</h3>
+        <p>Flower Earrings</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+    </div>
+  <?php } ?>
+  <!-- End of earrings -->
+
+  <!-- Display rings -->
+  <?php if ($product == "rings") { ?>
+    <div class="gallery">
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/purple-gemstone-ring.jpg" alt="purple-gemstone-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Purple Gemstone Ring</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/black-diamond-ring.jpg" alt="black-diamond-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Black Diamond Ring</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/gold-black-heart-ring.jpg" alt="gold-black-heart-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Gold & Black Heart Ring</p>
+        <h6>£20.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/infinite-hearts-ring.jpg" alt="infinite-hearts-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Infinite Hearts Ring</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/silver-love-ring.jpg" alt="silver-love-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Silver Love Ring</p>
+        <h6>£25.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/rings/joined-hearts-ring.jpg" alt="joined-hearts-ring" />
+        </div>
+        <h3>Ring</h3>
+        <p>Joined Hearts Ring</p>
+        <h6>£27.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+    </div>
+  <?php } ?>
+  <!-- End of rings -->
+
+  <!-- Display watches -->
+  <?php if ($product == "watches") { ?>
+    <div class="gallery">
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/pink-gold-bracelet-watch.jpg" alt="pink-gold-bracelet-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Pink & Gold Bracelet Watch</p>
+        <h6>£40.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/minimalistic-gold-white-watch.jpg" alt="minimalistic-gold-white-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Minimalistic Gold & White Watch</p>
+        <h6>£45.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/silver-grey-watch.jpg" alt="silver-grey-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Silver & Grey Watch</p>
+        <h6>£38.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/casual-timeless-watch.jpg" alt="casual-timeless-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Casual Timeless Watch</p>
+        <h6>£35.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/rose-gold-stars-watch.jpg" alt="rose-gold-stars-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Rose Gold Stars Watch</p>
+        <h6>£45.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+      <div class="content">
+        <div class="zoom">
+          <img src="images/shop/watches/wooden-strap-watch.jpg" alt="wooden-strap-watch" />
+        </div>
+        <h3>Watch</h3>
+        <p>Wooden Strap Watch</p>
+        <h6>£38.00</h6>
+        <button class="buy-1">Add to Basket</button>
+      </div>
+
+    </div>
+  <?php } ?>
+  <!-- End of watches -->
+
+</body>
+</html>
+
+<?php
+include('footer.php');
+?>
