eb3f3a855a578826d206ae9082b2d33f6a404482
Jawraj21 <210160435@aston.ac.uk>
2022-11-23 13:19:22 +0000

    Basket page changed

--- AstonJewellerss/basket.php
+++ AstonJewellerss/basket.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Aston Jewellers</title>
-    <link rel="icon" href="images/favicon.ico?v=2" type="image/x-icon" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-    />
-    <script
-      src="https://kit.fontawesome.com/c54d8b2ca0.js"
-      crossorigin="anonymous"
-    ></script>
-    <script defer src="js/main.js"></script>
-  </head>
 
-  <body>
-    <header id="main-header">
+<head>
+  <meta charset="utf-8" />
+  <title>Aston Jewellers</title>
+  <link rel="icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" />
+  <script src="https://kit.fontawesome.com/c54d8b2ca0.js" crossorigin="anonymous"></script>
+  <script defer src="js/main.js"></script>
+</head>
+
+<body>
+  <header id="main-header">
       <div class="topnav" id="nav">
-        <a href="#"><i class="fa fa-search"></i> Search</a>
-        <a href="#"><i class="fas fa-shipping-fast"></i> Shipping & Delivery info</a>
-        <div class="topnav-right">
-          <a href="#"><i class="fas fa-user-circle"></i> Log In</a>
+        <div class="container-fluid">
+          <div>
+            <a href="#"><i class="fa fa-search"></i> Search</a>
+          </div>
+          <div>
+            <h1>Aston Jewellers</h1>
+            <p>Selling high-quality affordable jewellery</p>
+          </div>
+          <div class="topnav-right">
+            <a href="login.php"><i class="fas fa-user-circle"></i> Log In</a>
+           
+          </div>
         </div>
-        <br /><br /><br />
-        <h1>Aston Jewellers</h1>
-        <p>Selling high-quality affordable jewellery</p>
-
         <nav>
           <div class="topnav-right">
             <a href="wishlist.php"><i class="fa fa-heart-o"></i></a>
@@ -36,10 +35,9 @@
           </div>
           <div id="topnav-centered">
             <ul>
-              <li><a href="index.php">New In</a></li>
+              <li><a href="index.php">Home</a></li>
               <li><a href="shop.php">Shop</a></li>
               <li><a href="#">Men's</a></li>
-              <li><a href="#">Bestsellers</a></li>
               <li><a href="sales.php">Sale</a></li>
               <li><a href="about-us.php">About Us</a></li>
             </ul>
