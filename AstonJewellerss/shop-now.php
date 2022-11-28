10b62c809eee6ebf790c04000b62c4ea4cb5a647
Jawraj21 <210160435@aston.ac.uk>
2022-11-28 21:48:30 +0000

    shop now page and css

---
+++ AstonJewellerss/shop-now.php
@@ -0,0 +1,31 @@
+<?php
+include './functions.php';
+?>
+
+<body>
+<?php
+loadHeader();
+?>
+    <main>
+    <div class="container">
+      <div class="shop-now">
+        <div id="img">
+            <div class="text">
+                <h3>Shop</h3>
+            </div>
+            <img class="product-img" src="images/shop_now/woman.jpg"alt="">
+        </div>
+        <div id="img">
+            <div class="text1">
+                <h3>Men's</h3>
+            </div>
+            <img src="images/shop_now/man.jpg" alt="">
+        </div>
+      </div>
+    </div>
+    </main>
+</body>
+
+<?php
+loadFooter();
+?>
